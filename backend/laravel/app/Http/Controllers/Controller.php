967592ffd86dda5aa3897010dc52707931bc7df3
RyosukeSakakibara <sakakibara.rs@impl.co.jp>
2024-07-26 20:59:42 +0900

    検証準備

--- backend/laravel/app/Http/Controllers/Controller.php
+++ backend/laravel/app/Http/Controllers/Controller.php
@@ -1,8 +1,20 @@
 <?php
 
-namespace App\Http\Controllers;
+declare(strict_types=1);
 
 abstract class Controller
 {
     //
 }
+
+class ExampleController extends Controller {
+    public function demo() {
+        echo "Hello, World!";
+    }
+}
+
+function add(int $a, int $b) {
+    return $a + $b;
+}
+
+echo add(2, '3'); // 出力: 5
