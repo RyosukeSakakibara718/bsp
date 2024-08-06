57a2219c507e52d38027d1300c1da1305a7906ae
yokohama.t <yokohama.t@impl.co.jp>
2024-08-06 21:18:31 +0900

    feat: メンバー情報一覧取得API作成

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types=1);
+
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('greeting', function() {
+Route::get('greeting', function () {
     return 'Hello, World';
 })->name('greeting');
+
+Route::get('/members', [MemberController::class, 'index']);
