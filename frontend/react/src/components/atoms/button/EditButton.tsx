f5d96caf469263a7cb219485119fa0aa23c1861d
RyosukeSakakibara <sakakibara.rs@impl.co.jp>
2024-08-15 23:23:10 +0900

    add: JSDocの追加

--- frontend/react/src/components/atoms/button/EditButton.tsx
+++ frontend/react/src/components/atoms/button/EditButton.tsx
@@ -5,6 +5,15 @@ type EditButtonProps = {
   onOpen: () => void
 };
 
+/**
+ * 編集ボタン
+ * 対象のメンバーの情報を編集するためのモーダルを開くための関数を呼び出す。
+ *
+ * @param {EditButtonProps} props - ボタンコンポーネントのプロパティ。
+ * @param {number} props.id - 編集対象のメンバーのID
+ * @param {function} props.onOpen - 編集モーダルを表示する関数。
+ * @returns {JSX.Element} 編集ボタンを開くためのボタン要素を返します。
+ */
 const EditButton: React.FC<EditButtonProps> = ({ id, onOpen }) => {
   return (
     <button onClick={() => onOpen()} className="bg-indigo-600 shadow text-white rounded-full py-1 px-4 hover:bg-indigo-800 transition-colors duration-300 ease-in-out">
