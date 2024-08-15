8e08ef3b34b76b9435e78681cb7740ec44d2632c
RyosukeSakakibara <sakakibara.rs@impl.co.jp>
2024-08-15 22:46:15 +0900

    記載内容の修正

--- frontend/react/src/components/atoms/Spacer.tsx
+++ frontend/react/src/components/atoms/Spacer.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-/**
- * @typedef {Object} SpacerProps
- * @property {string | number} height - 高さ。CSSの値として指定できます。
- */
 type SpacerProps = {
   height: string | number; // 高さを指定できるように
 };
@@ -13,6 +9,7 @@ type SpacerProps = {
  * 指定された高さ分のスペースを空けます。
  *
  * @param {SpacerProps} props - スペーサーコンポーネントのプロパティ。
+ * @param {number} props.height - 空白として設定する高さの値。
  * @returns {JSX.Element} 指定された高さ分のスペースを空けるためのdiv要素を返します。
  */
 const Spacer: React.FC<SpacerProps> = ({ height }) => {
