5b7d6bd521f2b3b4e9e03b4af5d0cc3b1bfdbd21
RyosukeSakakibara <sakakibara.rs@impl.co.jp>
2024-08-16 00:00:03 +0900

    add: JSDocの追加

--- frontend/react/src/components/atoms/column/TableHeaderColumn.tsx
+++ frontend/react/src/components/atoms/column/TableHeaderColumn.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 
 type TableHeaderColumnProps = {
   width?: string;
-  children: React.ReactNode;
+  value: string;
 };
 
-const TableHeaderColumn: React.FC<TableHeaderColumnProps> = ({ width, children }) => {
+/**
+ * テーブルヘッダーを構成するセルコンポーネント
+ *
+ * @param {TableHeaderColumnProps} props - セルコンポーネントに渡されるプロパティオブジェクト。
+ * @param {string} props.width - セルの幅。
+ * @param {function} props.value - セルに表示される値。
+ * @returns {JSX.Element} ヘッダーを構成するセルを返します。
+ */
+const TableHeaderColumn: React.FC<TableHeaderColumnProps> = ({ width, value }) => {
   return (
     <th 
       className="bg-gray-100 font-bold py-3 px-4 text-left border-b border-[#e1cfff] text-gray-800 whitespace-nowrap"
       style={{ width: width }}
     >
-      {children}
+      {value}
     </th>
   );
 };
