ed87612dfb71c9402a5589cde80a1033b4b94e68
tsukimi0618 <fall.frost.06@icloud.com>
2024-08-21 12:07:13 +0900

    add: JSDocの追加

--- frontend/react/src/components/atoms/column/TableHeaderColumn.tsx
+++ frontend/react/src/components/atoms/column/TableHeaderColumn.tsx
@@ -5,6 +5,15 @@ type TableHeaderColumnProps = {
   children: React.ReactNode;
 };
 
+/**
+ * テーブルヘッダーの列コンポーネント
+ * テーブルのヘッダーに表示する列を定義します。
+ * 
+ * @param {TableHeaderColumnProps} props - テーブルヘッダー列のプロパティ。
+ * @param {string} [props.width] - 列の幅を指定するスタイル属性。省略可能。
+ * @param {React.ReactNode} props.children - 列の内容となる子要素。通常はテキストやアイコンが含まれます。
+ * @returns {JSX.Element} テーブルヘッダーの列要素を返します。
+ */
 const TableHeaderColumn: React.FC<TableHeaderColumnProps> = ({ width, children }) => {
   return (
     <th 
