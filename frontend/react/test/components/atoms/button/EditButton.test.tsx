82b173290fb6bd9a5176cb0894848e84b57f89af
tsukimi0618 <fall.frost.06@icloud.com>
2024-08-21 09:31:40 +0900

    add:EditButtonのテストコード

---
+++ frontend/react/test/components/atoms/button/EditButton.test.tsx
@@ -0,0 +1,18 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import EditButton from "../../../../src/components/atoms/button/EditButton"
+import React from "react";
+
+describe('EditButtonコンポーネント',()=>{
+  it('ボタンが押された際にonOpenが正しく呼び出されるか確認する', ()=>{
+    const mockFunction = vi.fn(() => {});
+    const testId = 1;
+    render(<EditButton id={testId} onOpen={mockFunction} />)
+
+    const buttonElement = screen.getByRole('button')
+
+    fireEvent.click(buttonElement);
+    expect(mockFunction).toHaveBeenCalled();
+
+  })
+})
