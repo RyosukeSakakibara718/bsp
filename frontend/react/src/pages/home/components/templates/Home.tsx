946ef1bf5a6d6cc56b3d23c6a381a0453f35b14f
yamazaki <1521ttyy@gmail.com>
2024-08-31 16:00:54 +0900

    feat:route create

---
+++ frontend/react/src/pages/home/components/templates/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      {/* 詳細ページのコンテンツ */}
+      Home画面を表示します。
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/react/src/pages/userManagement/components/templates/UserManagement.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserManagement: React.FC = () => {
+  return (
+    <div>
+      {/* 詳細ページのコンテンツ */}
+      usermanagement画面を表示します。
+    </div>
+  );
+};
+
+export default UserManagement;
