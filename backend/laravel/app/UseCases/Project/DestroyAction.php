f1ee5982fafa54fd6768b2bf691eb94f3357adab
tsukimi0618 <fall.frost.06@icloud.com>
2024-09-02 12:16:32 +0900

    add:削除機能の作成

---
+++ backend/laravel/app/UseCases/Project/DestroyAction.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UseCases\Project;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class DestroyAction
+{
+    public function __invoke(string $id): void
+    {
+        // プロジェクトをIDで検索
+        $project = Project::find($id);
+
+        if (!$project) {
+            throw new ModelNotFoundException('Project not found');
+        }
+
+        // プロジェクトを削除
+        $project->delete();
+    }
+}
