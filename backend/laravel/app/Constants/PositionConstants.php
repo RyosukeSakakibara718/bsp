55dc502c44781cbeb690534640b3749ab053f041
nara.t <nara.t@impl.co.jp>
2024-09-12 00:25:49 +0900

    add:ポジションの定数にQAも追加

--- backend/laravel/app/Constants/PositionConstants.php
+++ backend/laravel/app/Constants/PositionConstants.php
@@ -7,11 +7,12 @@ class PositionConstants
     const POSITION_PM = 1;
     const POSITION_MEMBER = 2;
     const POSITION_MGR = 3;
+    const POSITION_QA = 4;
 
     /**
      * Undocumented function
      *
-     * @param [type] $position  PM = 1, メンバー = 2, MGR = 3
+     * @param [type] $position  PM = 1, メンバー = 2, MGR = 3, QA = 4
 
      * @return void
      */
@@ -21,6 +22,7 @@ class PositionConstants
             self::POSITION_PM => 'PM',
             self::POSITION_MEMBER => 'メンバー',
             self::POSITION_MGR => 'MGR',
+            self::POSITION_QA => 'QA',
         ];
 
         return $positions[$position] ?? '不明';
