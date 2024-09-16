2bc7e99ce5c67e0726af49400e76de97cc826dd7
RyosukeSakakibara <sakakibara.rs@impl.co.jp>
2024-09-16 19:06:49 +0900

    案件実績画面 モックデータ作成

---
+++ frontend/react/src/data/projectsArchivements.ts
@@ -0,0 +1,130 @@
+export const sampleProjectArchivementsData = {
+  "projects": {
+    "projects_id": 1,
+    "assignment_members": [
+      {
+        assignment_member_id: 1,
+        position: 1,
+        work_costs: [
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/01",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/02",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/03",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/04",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/05",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/06",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/07",
+          },
+        ],
+      },
+      {
+        assignment_member_id: 2,
+        position: 1,
+        work_costs: [
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/01",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/02",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/03",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/04",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/05",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/06",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/07",
+          },
+        ],
+      },
+      {
+        assignment_member_id: 3,
+        position: 1,
+        work_costs: [
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/01",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/02",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/03",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/04",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/05",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/06",
+          },
+          {
+            daily_cost: 40000,
+            work_time: 8,
+            work_date: "2024/01/07",
+          },
+        ],
+      },
+    ],
+  }
+};
