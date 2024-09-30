f5a755653017eec336456f17606396a76923d679
yokohama.t <yokohama.t@impl.co.jp>
2024-09-30 17:57:57 +0900

    feat: seeder,factory作成

--- backend/laravel/database/factories/CommentFactory.php
+++ backend/laravel/database/factories/CommentFactory.php
@@ -3,12 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Comment;
+use App\Models\Project;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
  */
 class CommentFactory extends Factory
 {
+    protected $model = Comment::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +20,11 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'project_id' => Project::inRandomOrder()->first()->id,
+            'comment'    => $this->faker->realText(200), // コメントの内容をランダムに生成
+            'created_at' => now(),
+            'updated_at' => now(),
+            'deleted_at' => null,
         ];
     }
 }
