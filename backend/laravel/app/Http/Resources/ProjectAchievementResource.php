c333e6d7b55c5c26bfb7adf05de6a69a18aa5564
tsukimi0618 <fall.frost.06@icloud.com>
2024-10-11 05:47:22 +0900

    Revert "Revert "案件実績入力画面 / バックエンド実装 / 案件実績詳細取得API作成""
    This reverts commit c21f6657083e839c58b7d44013ac0af6764e35ef.

---
+++ backend/laravel/app/Http/Resources/ProjectAchievementResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectAchievementResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'project' =>[
+                'id' => $this->id,
+                'assignment_members' => [
+                    AchievementAssignmentMemberResource::collection($this->assignmentMembers)
+                ]
+            ]
+        ];
+    }
+
+}
