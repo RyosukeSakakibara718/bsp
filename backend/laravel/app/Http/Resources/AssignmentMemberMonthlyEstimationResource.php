0c310eedcd666b38cade577c436b07075702ccaa
yokohama.t <yokohama.t@impl.co.jp>
2024-10-14 23:32:31 +0900

    fix: estimate_costの追加

--- backend/laravel/app/Http/Resources/AssignmentMemberMonthlyEstimationResource.php
+++ backend/laravel/app/Http/Resources/AssignmentMemberMonthlyEstimationResource.php
@@ -16,8 +16,8 @@ class AssignmentMemberMonthlyEstimationResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'assignment_member_id' => $this->assignment_member_id,
             'target_month' => $this->target_month,
+            'estimate_cost' => $this->estimate_cost,
             'estimate_person_month' => $this->estimate_person_month !== null ? floatval($this->estimate_person_month) : 0,
         ];
     }
