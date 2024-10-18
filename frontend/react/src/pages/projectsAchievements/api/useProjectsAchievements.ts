8c6807d46b8b576ee466cc0b43871b18b14f5f9b
RyosukeSakakibara718 <vala_0718@i.softbank.jp>
2024-10-19 03:14:47 +0900

    編集リクエスト準備まで

---
+++ frontend/react/src/pages/projectsAchievements/api/useProjectsAchievements.ts
@@ -0,0 +1,15 @@
+import { ProjectAchievementsData } from "../../../types/project";
+
+export const getProjectsAchievements = (id: number): Promise<ProjectAchievementsData> => {
+  return fetch(`http://localhost/v1/projectsAchievements/${id}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('data.projects: ', data.project);
+      return data;
+    });
+};
